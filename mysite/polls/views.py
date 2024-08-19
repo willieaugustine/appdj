c5d0b83eb8f168e3b49075fe03c231c4bbb58472
Willieaugustine <mulwawillie@gmail.com>
2024-08-19 16:34:41 +0300

    Changes to views

--- mysite/polls/views.py
+++ mysite/polls/views.py
@@ -1,4 +1,6 @@
+from typing import Any
 from django.db.models import F
+from django.db.models.query import QuerySet
 from django.http import HttpResponseRedirect
 from django.shortcuts import get_object_or_404, render
 from django.urls import reverse
@@ -12,10 +14,14 @@ class IndexView(generic.ListView):
     template_name = "polls/index.html"
     context_object_name = "latest_question_list"
     def get_queryset(self):
-        return Question.objects.order_by("-pub_date")[:5]
+        return Question.objects.filter(pub_date_lte=timezone.now()).order_by("-pub_date")[
+            :5
+        ]
 class DetailView(generic.DetailView):
     model = Question
     template_name = "polls/detail.html"
+    def get_queryset(self):
+        return Question.objects.filter(pub_date_lte=timezone.now())
 class ResultsView(generic.DetailView):
     model = Question
     template_name = "polls/results.html"
